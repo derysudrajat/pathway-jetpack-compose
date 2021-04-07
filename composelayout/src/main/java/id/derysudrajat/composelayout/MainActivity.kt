34443e18da5d09eb848439b6b16bad85b50140bf
derysudrajat <dery.sudrajat17@gmail.com>
2021-04-07 23:45:46 +0700

    add: working with lists

--- composelayout/src/main/java/id/derysudrajat/composelayout/MainActivity.kt
+++ composelayout/src/main/java/id/derysudrajat/composelayout/MainActivity.kt
@@ -5,10 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,13 +15,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.coil.CoilImage
 import id.derysudrajat.composelayout.ui.theme.JetpackComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -143,3 +145,71 @@ fun ScaffoldLayoutPreview() {
         ScaffoldLayout()
     }
 }
+
+@Composable
+fun LazyList() {
+    Scaffold { innerPadding ->
+        val listSize = 100
+        // we want to save the scrolling position with this state that can also
+        // be used to programmatically scroll the list
+        val scrollState = rememberLazyListState()
+        // we want to save the coroutine scope where out animated scroll will be executed
+        val coroutineScope = rememberCoroutineScope()
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(8.dp)
+            ) {
+                Button(onClick = {
+                    coroutineScope.launch {
+                        // 0 is the first item index
+                        scrollState.animateScrollToItem(0)
+                    }
+                }, Modifier.weight(1f)) {
+                    Text(text = "Scroll to the top")
+                }
+                Spacer(modifier = Modifier.size(8.dp))
+                Button(onClick = {
+                    coroutineScope.launch {
+                        // listSize - 1 is the last index of the list
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }, Modifier.weight(1f)) {
+                    Text(text = "Scroll to the end")
+                }
+            }
+            LazyColumn(state = scrollState) {
+                items(listSize) {
+                    ImageListItem(index = it, Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.padding(16.dp)
+    ) {
+        CoilImage(
+            data = "https://developer.android.com/images/brand/Android_Robot.png",
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview
+@Composable
+fun LazyListPreview() {
+    JetpackComposeTheme {
+        LazyList()
+    }
+}
+
